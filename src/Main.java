dd26c01d0f58d0129aed071927521fee9f149b0c
Elya <elyasalmanovaasr@gmail.com>
2024-11-14 23:02:09 +0700

    code

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        int currentBalance = 389;// текущий баланс
-        int amountUp = 10000; // сумма пополнения
+        int currentBalance = 100;// текущий баланс
+        int amountUp = 300; // сумма пополнения
 
 
-        if (amountUp >= 1000) ; // проверяем условие о пополнении на сумму свыше 1000
-        int bonus = (amountUp / 100);// считаем бонус
+        int bonus = 0;             // начальное значение бонуса
+        int totalBalance = 0;      // итоговый баланс
 
-        int totalBalance = currentBalance + bonus + amountUp;// итоговый баланс
+        if (amountUp >= 1000) {
+            bonus = (amountUp / 100);//формула расчета бонусов
+        }
+
+        totalBalance = currentBalance + bonus + amountUp;//формула расчета итогового баланса при начислении бонусов
 
         if (bonus > 0) {
             System.out.println("Начислено " + bonus + " бонусных руб."); // Сообщение о начисленных бонусах
             System.out.println("Итоговый баланс: " + totalBalance + " руб."); // Итоговый баланс при начислении бонусов
         } else {
-            System.out.println("Бонусные рубли отсутствуют."); // Сообщение о том, что бонусы отсутствуют
+            System.out.println("Бонусные рубли отсутствуют.");
             System.out.println("Итоговый баланс: " + totalBalance + " руб."); // Итоговый баланс без бонусов
         }
     }
